15d57b95e55e68167f325a8ccb62ee8a2524fa73
Sxg0673 <santiago.castillo@uao.edu.co>
2025-05-19 15:18:15 -0500

    Primer commit del proyecto

--- visualizacion.py
+++ visualizacion.py
@@ -6,10 +6,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from analisis import DataAnalyzer
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
 
-df = pd.read_csv("adult.csv")
+data = pd.read_csv("adult.csv")
 
-analizar = DataAnalyzer(df)
-analizar.summary()
-analizar.correlation_matrix()
-analizar.categorical_analisis()
+analizar = DataAnalyzer(data)
+def informacion():
+    try: 
+        text_area.delete(1.0, tk.END) #para vaciar al ejecutar
+        info = analizar.summary()
+        text_area.insert(tk.END, info)
+    except Exception as e:
+        traceback.print_exc()
+        messagebox.showerror("Error", "No se puede")
+
+ventana = tk.Tk()
+ventana.title("Analisis de datos")
+
+boton_summary = tk.Button(ventana, text="Info", command=informacion)
+boton_summary.pack()
+
+text_area = ScrolledText(ventana,width=70, height=30)
+
+text_area.pack()
+
+ventana.mainloop()
